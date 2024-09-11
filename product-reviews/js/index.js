281ca14a71d1c6840868fb4334ad23393ff097bd
Samah <samahalsulami15@gmail.com>
2024-09-11 17:27:23 +0300

    adding API data

--- product-reviews/js/index.js
+++ product-reviews/js/index.js
@@ -1,2 +1,15 @@
-// Write custom JavaScript here.
-// You may ignore this file and delete if if JavaScript is not required for your challenge.
+// Fetch data from API 
+fetch('https://www.greatfrontend.com/api/projects/challenges/e-commerce/products/:productId/reviews')
+    .then(response => {
+        if(!response.ok){
+            throw new Error("Couldn't fetch API, Network issue")
+        }
+        return response.json()
+    })
+    .then(data =>{
+        console.log(data)
+        console.log(data.aggregate.counts[0].count)
+    })
+    .catch(error =>{
+        console.error('There was a problem with the fetch operation:', error);
+    })
