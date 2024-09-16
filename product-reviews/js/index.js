cda18cd51d812b605b380c07648de65fae2d753e
Samah <samahalsulami15@gmail.com>
2024-09-16 23:37:42 +0300

    'updates'

--- product-reviews/js/index.js
+++ product-reviews/js/index.js
@@ -1,4 +1,7 @@
 // Fetch data from API 
+let storedData
+
+// Fetch function 
 fetch('https://www.greatfrontend.com/api/projects/challenges/e-commerce/products/:productId/reviews')
     .then(response => {
         if(!response.ok){
@@ -8,8 +11,43 @@ fetch('https://www.greatfrontend.com/api/projects/challenges/e-commerce/products
     })
     .then(data =>{
         console.log(data)
-        console.log(data.aggregate.counts[0].count)
+        //console.log(data.aggregate.counts[1].count)
+        storedData = data; 
     })
     .catch(error =>{
         console.error('There was a problem with the fetch operation:', error);
     })
+
+// Display data on overall ratings 
+// Get elements by their IDs
+const excellentPercentage = document.getElementById('excellent-percentage');
+const goodPercentage = document.getElementById('good-percentage');
+const averagePercentage = document.getElementById('average-percentage');
+const belowAveragePercentage = document.getElementById('below-average-percentage');
+const poorPercentage = document.getElementById('poor-percentage');
+
+// Set percentages values 
+
+const percentages = {
+    excellent: 90,
+    good: 75,
+    average: 50,
+    below_average: 45, 
+    poor: 20
+};
+
+// call functio 
+updatePercentages()
+
+
+// Function to update percentages in the DOM
+function updatePercentages(){
+    excellentPercentage.textContent = `${percentages.excellent}%`; 
+    goodPercentage.textContent = `${percentages.good}%`;
+    averagePercentage.textContent = `${percentages.average}%`;
+    belowAveragePercentage.textContent = `${percentages.below_average}%`;
+    poorPercentage.textContent = `${percentages.poor}%`;
+
+    // Update percentages bars 
+}
+
